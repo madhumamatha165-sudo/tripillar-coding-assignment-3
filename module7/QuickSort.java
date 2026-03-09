00e42b9cdf354009a9da78130035ced95cbf6213
Mamatha J <madhumamatha165@gmail.com>
2026-03-09 22:43:11 +0530

    Create QuickSort.java

---
+++ module7/QuickSort.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void main(String[] args) {
+
+        int[] arr={5,2,4,6,1};
+
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
