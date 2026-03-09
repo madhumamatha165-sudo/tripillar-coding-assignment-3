d4b8da18ff77edcf8f4489982af90b12bde19dd6
Mamatha J <madhumamatha165@gmail.com>
2026-03-09 22:31:52 +0530

    Create DifferenceArray.java

---
+++ module6/DifferenceArray.java
@@ -0,0 +1,15 @@
+public class DifferenceArray {
+    public static void main(String[] args) {
+        int[] arr = {10,10,10,10,10};
+        int[] diff = new int[arr.length];
+
+        diff[0] = arr[0];
+
+        for(int i=1;i<arr.length;i++){
+            diff[i] = arr[i] - arr[i-1];
+        }
+
+        for(int i:diff)
+            System.out.print(i+" ");
+    }
+}
