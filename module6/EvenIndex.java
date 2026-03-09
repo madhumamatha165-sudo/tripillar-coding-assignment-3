ff540e8239b0da7a11a99e2883bdc6ab35cf2a9f
Mamatha J <madhumamatha165@gmail.com>
2026-03-09 22:36:42 +0530

    Create EvenIndex.java

---
+++ module6/EvenIndex.java
@@ -0,0 +1,11 @@
+public class EvenIndex {
+    public static void main(String[] args) {
+        int[] arr = {3,6,12,1,5,8};
+
+        for(int i=0;i<arr.length;i++){
+            if(i%2==0 && arr[i]%2!=0){
+                System.out.println("Mismatch at index "+i);
+            }
+        }
+    }
+}
